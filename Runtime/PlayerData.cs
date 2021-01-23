98685a14abf135afd6074a7f167026aef08de8e4
Hubert GROSSIN <hubert.grossin@gmail.com>
2021-01-23 16:52:52 +0100

    Add IsPlaying var

--- Runtime/PlayerData.cs
+++ Runtime/PlayerData.cs
@@ -15,6 +15,10 @@ namespace CustomPackage
         private int m_Score = 0;
         public int Score => m_Score;
 
+        [SerializeField]
+        private bool m_IsPlaying = false;
+        public bool IsPlaying => m_IsPlaying;
+
     }
 
 }
